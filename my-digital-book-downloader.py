82ee31dd1e77b7fe4adce4f4faa2c58f76e0b2fe
Kumade23 <scogna267@gmail.com>
2024-08-23 15:56:58 +0200

    Update my-digital-book-downloader.py

--- my-digital-book-downloader.py
+++ my-digital-book-downloader.py
@@ -3,9 +3,8 @@ import requests
 import json
 import uuid
 import os
-import zipfile
-import tempfile
 from tqdm import tqdm
+from remotezip import RemoteZip
 
 session = tls_client.Session(
     client_identifier="safari_ios_15_5",
@@ -55,8 +54,6 @@ for titolo, id in titoli_id:
 
 id_scelto = input("\nID : ")
 
-print("\nFetching data...\n")
-
 random_uuid = uuid.uuid4()
 uuid_u = str(random_uuid).upper()
 
@@ -70,37 +67,13 @@ url_download = libri_download.json().get('result', {}).get('data', {}).get('url_
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-def download_with_progress(url_download, filepath):
-    response = requests.get(url_download, stream=True)
-    total_size = int(response.headers.get('content-length', 0))
-    chunk_size = 1024
-    with open(filepath, 'wb') as file, tqdm(
-        total=total_size,
-        unit='B',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        for data in response.iter_content(chunk_size=chunk_size):
-            file.write(data)
-            bar.update(len(data))
+print("\nFetching data...\n")
 
-with tempfile.TemporaryDirectory() as tempdir:
-    zip_file_path = os.path.join(tempdir, 'downloaded_file.zip')
-    
-    download_with_progress(url_download, zip_file_path)
-    
-    extract_folder = os.path.join(tempdir, 'extracted_data')
-    os.makedirs(extract_folder, exist_ok=True)
-    
-    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_folder)
-    
-    master_json_path = os.path.join(extract_folder, 'data', 'master.json')
-    with open(master_json_path, 'r') as json_file:
-        master_data = json.load(json_file)
+with RemoteZip(url_download) as zip_file:
+    with zip_file.open('data/master.json') as master_json_file:
+        master_data = json.load(master_json_file)
 
-
-print("\nDownloading pdf...\n")
+print("\nDownloading PDF...\n")
 
 def trova_link_pdf(master_data):
     for unit in master_data['units']:
@@ -116,6 +89,20 @@ pdf_url = trova_link_pdf(master_data)
 pdf_na = master_data['name']
 pdf_nam = pdf_na.replace("/", "-")
 pdf_name = f"{pdf_nam}.pdf"
+
+def download_with_progress(url_download, filepath):
+    response = requests.get(url_download, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    chunk_size = 1024
+    with open(filepath, 'wb') as file, tqdm(
+        total=total_size,
+        unit='B',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in response.iter_content(chunk_size=chunk_size):
+            file.write(data)
+            bar.update(len(data))
 
 pdf_finale = os.path.join(current_directory, pdf_name)
 download_with_progress(pdf_url, pdf_finale)
